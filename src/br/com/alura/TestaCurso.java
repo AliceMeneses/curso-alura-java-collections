c83c6665eecf11f9004be53b70d26f71efff347b
Alice <am171696@alunos.unisanta.br>
2020-11-17 12:53:39 -0300

    Manipulando cursos

---
+++ src/br/com/alura/TestaCurso.java
@@ -0,0 +1,10 @@
+package br.com.alura;
+
+public class TestaCurso {
+
+	public static void main(String[] args) {
+		Curso javaColecoes = new Curso("Dominando Coleções com Java", "Alice Meneses");
+		System.out.println(javaColecoes.getAulas());
+	}
+
+}
