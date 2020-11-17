d40afd295669fdee8ada465b590f0dfa5a0b8ce2
Alice <am171696@alunos.unisanta.br>
2020-11-17 12:39:15 -0300

    Gerando classe Curso

---
+++ src/br/com/alura/Curso.java
@@ -0,0 +1,29 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Curso {
+	
+	private String instrutor;
+	private String nome;
+	private List<Aula> aulas = new ArrayList<>();
+	
+	public Curso(String instrutor, String nome) {
+		this.instrutor = instrutor;
+		this.nome = nome;
+	}
+	
+	public String getInstrutor() {
+		return instrutor;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public List<Aula> getAulas() {
+		return aulas;
+	}
+	
+}
