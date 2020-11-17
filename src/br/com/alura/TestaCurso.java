d9142f39cab3cdaa4fd4e45a4b89948007abf9a2
Alice <am171696@alunos.unisanta.br>
2020-11-17 13:39:14 -0300

    Testando exceção UnsupportedOperationException

--- src/br/com/alura/TestaCurso.java
+++ src/br/com/alura/TestaCurso.java
@@ -4,7 +4,21 @@ public class TestaCurso {
 
 	public static void main(String[] args) {
 		Curso javaColecoes = new Curso("Dominando Coleções com Java", "Alice Meneses");
+		
+		javaColecoes.adiciona(new Aula("Manipulando Array",10));
+		javaColecoes.adiciona(new Aula("Diferença entre LinkedList e ArrayList",15));
+		javaColecoes.adiciona(new Aula("Vector",5));
+		
 		System.out.println(javaColecoes.getAulas());
+		
+		try{
+		javaColecoes.getAulas().remove(0);
+		}
+		catch(UnsupportedOperationException ex) {
+			System.out.println();
+			System.out.println("Exceção ao tentar modificar os elementos de uma lista que não permite ser modificada");
+			ex.printStackTrace();
+		}
 	}
 
 }
