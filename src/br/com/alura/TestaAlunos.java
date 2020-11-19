553f1623d652b912de8c4dbcb71f10a171f23796
Alice <am171696@alunos.unisanta.br>
2020-11-19 19:16:28 -0300

    Manipulando conjunto de alunas

---
+++ src/br/com/alura/TestaAlunos.java
@@ -0,0 +1,21 @@
+package br.com.alura;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestaAlunos {
+
+	public static void main(String[] args) {
+		Set<String> alunos = new HashSet();
+		
+		alunos.add("Alice Meneses");
+		alunos.add("Gabriella");
+		alunos.add("Maria");
+		alunos.add("Julianne");
+		alunos.add("Isa");
+		
+		System.out.println(alunos);
+		
+	}
+
+}
